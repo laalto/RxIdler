75af51ce8be7d9298711ba2b39d2bce58404f9a1
Lauri Aalto <laalto@gmail.com>
2020-03-10 12:58:15 +0200

    Port rx2-idler to rxjava3 as rx3-idler
    Needed to update to Java 8 for rxjava3's static interface methods like Disposable.disposed()

---
+++ rx3-idler/src/test/java/com/squareup/rx3/idler/Rx3IdlerTest.java
@@ -0,0 +1,34 @@
+package com.squareup.rx3.idler;
+
+import org.junit.Test;
+
+import io.reactivex.rxjava3.schedulers.TestScheduler;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public final class Rx3IdlerTest {
+  @Test public void createNullArgumentsFlow() {
+    try {
+      Rx3Idler.create(null);
+      fail();
+    } catch (NullPointerException e) {
+      assertEquals("name == null", e.getMessage());
+    }
+  }
+
+  @Test public void nullWrapArgumentsFails() {
+    try {
+      Rx3Idler.wrap(null, "Bob");
+      fail();
+    } catch (NullPointerException e) {
+      assertEquals("scheduler == null", e.getMessage());
+    }
+    try {
+      Rx3Idler.wrap(new TestScheduler(), null);
+      fail();
+    } catch (NullPointerException e) {
+      assertEquals("name == null", e.getMessage());
+    }
+  }
+}
